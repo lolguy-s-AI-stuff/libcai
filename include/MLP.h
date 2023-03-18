5768fafc73586fc683b0b10e35159990fc287746
lol <gge>
2023-03-18 14:58:27 +0100

    destupidification and documentation

--- include/MLP.h
+++ include/MLP.h
@@ -2,20 +2,54 @@ namespace CAI
 {
     namespace MLP
     {
-    struct MlpData{//data like topology, weights and biases, activation functions
+    /// \brief A struct that holds the data of a MLP
+    struct MlpData{
         int num_layers;
         int *topology;
         double **weights;
         double **biases;
-        int *activation_functions;
+        int activation_function;
     }
-    void Run(const char* input,MlpData model,const char* out_to_here);        
 
+    /**
+     * @brief Run the neural network
+     * @param input The input of the neural network
+     * @param model The model 
+     * @param out_to_here The output of the neural network
+     * @param num_inputs The number of inputs
+     */
+    void Run(const char* input,MlpData model,const char* out_to_here,int num_inputs);
+
+    /**
+     * @brief Create an empty MlpData
+     * @param num_layers number of layers
+     * @param topology array of int containing the number of neurons in each layer
+     * @return an empty MlpData
+     */
     MlpData CreateEmpty(int num_layers,int *topology);
 
+    /**
+     * @brief Edit a weight of the MlpData
+     * @param model the MlpData to edit
+     * @param layer the layer of the weight
+     * @param address the address of the weight
+     * @param value the new value of the weight
+     */
     void EditWeight(MlpData model, int layer,int address,int value);
+
+    /**
+     * @brief Edit a bias of the MlpData
+     * @param model the MlpData to edit
+     * @param layer the layer of the bias
+     * @param address the address of the bias
+     * @param value the new value of the bias
+     */
     void EditBias(MlpData model, int layer,int address,int value);
 
+    /**
+     * @brief Free the MlpData
+     * @param tofree the MlpData to free
+     */
     void Free(MlpData tofree);
     }
 
